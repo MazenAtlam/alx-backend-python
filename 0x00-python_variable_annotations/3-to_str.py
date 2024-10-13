7cea2ac59f0bcb3b66858a8d3cb0de71c4f3cc4a
MazenAtlam <mazenatef5510@gmail.com>
2024-10-13 22:53:45 +0300

    Basic annotations - to string-task 3

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+"""A module has a to_str function that gets the representation of the float"""
+
+
+def to_str(n: float) -> str:
+    """A  type-annotated function that gets the representation of the float
+
+    Args:
+        n (float): The float number
+
+    Returns:
+        str: The string representation of the float
+    """
+
+    return str(n)
