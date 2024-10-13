d129baf4dc3fae654215693f3cb0e01ed6185ed3
MazenAtlam <mazenatef5510@gmail.com>
2024-10-13 23:28:43 +0300

    rename the argument- task 6 v1.1

--- 0x00-python_variable_annotations/6-sum_mixed_list.py
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -7,14 +7,14 @@ returns the sum of a list of numbers
 from typing import List
 
 
-def sum_mixed_list(mxd_list: List[float, int]) -> float:
+def sum_mixed_list(mxd_lst: List[float, int]) -> float:
     """A type-annotated function that calculates the sum of a list of numbers
 
     Args:
-        mxd_list (List[float, int]): A list of numbers
+        mxd_lst (List[float, int]): A list of numbers
 
     Returns:
         float: The sum of the numbers
     """
 
-    return sum(mxd_list)
+    return sum(mxd_lst)
