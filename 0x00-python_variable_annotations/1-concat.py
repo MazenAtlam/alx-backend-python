5041db8dc6a0e0eb6e835715556364a999d83169
MazenAtlam <mazenatef5510@gmail.com>
2024-10-13 22:26:52 +0300

    Basic annotations - concat-task 1

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+"""A module has a concat function that returns a concatenated string"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """A type-annotated function that returns a concatenated string
+
+    Args:
+        str1 (str): The first string
+        str2 (str): The second string
+
+    Returns:
+        str: The concatenated string
+    """
+
+    return str1 + str2
