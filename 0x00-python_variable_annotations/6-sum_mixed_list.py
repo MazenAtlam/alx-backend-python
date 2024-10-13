35c2c840a6a88c83f67fb0590e3a303b5db7d699
MazenAtlam <mazenatef5510@gmail.com>
2024-10-13 23:39:52 +0300

    using Union from typing-task 6 2.0

--- 0x00-python_variable_annotations/6-sum_mixed_list.py
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -4,10 +4,10 @@
 returns the sum of a list of numbers
 """
 
-from typing import List
+from typing import List, Union
 
 
-def sum_mixed_list(mxd_lst: List[float, int]) -> float:
+def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
     """A type-annotated function that calculates the sum of a list of numbers
 
     Args:
