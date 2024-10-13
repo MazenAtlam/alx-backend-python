1c17451c22a04e8fe15df6854f25c6172d813d37
MazenAtlam <mazenatef5510@gmail.com>
2024-10-13 22:45:45 +0300

    Basic annotations - floor-task 2

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+"""A module has an add function that returns the sum of two floats"""
+
+
+def floor(n: float) -> int:
+    """A type-annotated function that returns the floor of the float number
+
+    Args:
+        n (float): The float number
+
+    Returns:
+        int: The floor of the float number
+    """
+
+    return int(n // 1)
