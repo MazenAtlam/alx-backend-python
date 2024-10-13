dbef06954f843ab8a8e06b2a52409d9cd2907cec
MazenAtlam <mazenatef5510@gmail.com>
2024-10-13 22:14:05 +0300

    Basic annotations - add-task 0

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+"""A module has an add function that returns the sum of two floats"""
+
+
+def add(num1: float, num2: float) -> float:
+    """A type-annotated function that calculates the sum of two numbers
+
+    Args:
+        num1 (float): The first number
+        num2 (float): The second number
+
+    Returns:
+        float: The sum of the two numbers
+    """
+
+    return num1 + num2
