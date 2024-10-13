ff5fe04bd7bd8ac09db454271013f76716abd3b5
MazenAtlam <mazenatef5510@gmail.com>
2024-10-14 00:38:46 +0300

    TypeVar-task 11

---
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+
+"""A module has an function to be annotated"""
+
+from typing import TypeVar, Mapping, Any, Union
+
+
+T = TypeVar('T')
+
+def safely_get_value(dct: Mapping, key: Any, default: Union[T, None] = None):
+    """A function that gets the first element of a dictionary
+
+    Args:
+        dct (Mapping): A dictionary of keys and values
+        key (Any): The key of the element
+        default: The default key
+
+    Returns:
+        Union[T, None]: The first element or None if it is empty
+    """
+
+    if key in dct:
+        return dct[key]
+    else:
+        return default
