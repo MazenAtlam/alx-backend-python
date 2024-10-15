f1f136170be6107cccaeea81c373da9468f41193
MazenAtlam <mazenatef5510@gmail.com>
2024-10-15 23:45:20 +0300

    fix warnings-task 2 v1.1

--- 0x01-python_async_function/2-measure_runtime.py
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -2,12 +2,13 @@
 
 """A module used to measure the total execution time"""
 
+import asyncio
 import time
 
 wait_n = __import__('1-concurrent_coroutines').wait_n
 
 
-async def measure_time(n: int, max_delay: int) -> float:
+def measure_time(n: int, max_delay: int) -> float:
     """A function that measures the total execution time for wait_n
 
     Args:
@@ -19,7 +20,7 @@ async def measure_time(n: int, max_delay: int) -> float:
     """
 
     t0: float = time.time()
-    await wait_n(n, max_delay)
+    asyncio.run(wait_n(n, max_delay))
     t1: float = time.time()
 
     total_time: float = t1 - t0
